c581e11d2be8dd962b7b5676553736dabb931f7a
Juan Gabriel <jgvelort1611@gmail.com>
2020-11-08 14:24:29 -0500

    Estructura Inicial Proyecto back Maxi entregas api

---
+++ src/main/java/co/udea/maxi/entregas/api/MaxiEntregasApiApplication.java
@@ -0,0 +1,13 @@
+package co.udea.maxi.entregas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxiEntregasApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaxiEntregasApiApplication.class, args);
+    }
+
+}
